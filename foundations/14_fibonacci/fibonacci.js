2ca26e615a6011cc4111165fa67d2bb171ed8244
mynamesdallas <62816012+mynamesdallas@users.noreply.github.com>
2026-07-10 12:23:24 -0400

    Used the solution to do this one

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let count;
+    if (typeof n !== 'number') {
+        count = parseInt(n);
+    } else {
+        count = n;
+    }
 
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return firstPrev;
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
